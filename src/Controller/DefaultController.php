9b6c4408d1b11c9aca48fa90967163152d09415e
hrobben <hrobben@robusoft.com>
2019-05-28 13:29:42 +0200

    update

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -21,19 +21,35 @@ class DefaultController extends AbstractController
     public function index()
     {
         if ($this->security->isGranted('ROLE_ADMIN')) {
+            $checkTime = new \DateTime("now");
+            $checkTime->modify('- 10 minutes');
+
+            $repository = $this->getDoctrine()
+                ->getRepository('App:User');
+
+            $query = $repository->createQueryBuilder('p')
+                ->where('p.lastActivityAt > :checkTime')
+                ->setParameter('checkTime', $checkTime)
+                ->getQuery();
+
+            $users = $query->getResult();
+
             return $this->render('default/index.html.twig', [
                 'controller_name' => 'DefaultController',
                 'role' => 'ADMIN',
+                'users' => $users,
             ]);
         } elseif ($this->security->isGranted('ROLE_USER')) {
             return $this->render('default/index.html.twig', [
                 'controller_name' => 'DefaultController',
                 'role' => 'USER',
+                'users' => null
             ]);
         } else {
             return $this->render('default/index.html.twig', [
                 'controller_name' => 'DefaultController',
                 'role' => 'NONE',
+                'users' => null,
             ]);
 
         }
